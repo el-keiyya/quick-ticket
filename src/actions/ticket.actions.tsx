15750bacfb471992c1e66702b30837f9fe6b7284
keiyya <keiyyaalex@gmail.com>
2025-07-11 15:09:47 +0300

    useActionState

--- src/actions/ticket.actions.tsx
+++ src/actions/ticket.actions.tsx
@@ -1,6 +1,9 @@
 "use server";
 
-export async function createTicket(formData: FormData) {
+export async function createTicket(
+  prevState: { success: boolean; message: string },
+  formData: FormData
+): Promise<{ success: boolean; message: string }> {
   const subject = formData.get("subject") as string;
   const description = formData.get("description") as string;
   const priority = formData.get("priority") as string;
